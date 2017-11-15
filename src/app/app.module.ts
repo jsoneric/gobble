deb312de6ea522bd414710652a5743a2653050e8
Jason Hammond <jhammond@paylock.com>
2017-11-15 12:27:10 -0500

    module additions. config changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,46 @@
+import { DBService } from './../shared/services/db-service';
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
 import { Timers } from '../pages/timers/timers';
-import { TimerDetail } from '../pages/timer-detail/timer-detail';
+import { EditTimer } from '../pages/edit-timer/edit-timer';
 import { AddTimer } from '../pages/add-timer/add-timer';
+import { TimerService } from '../shared/services/timer-service';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { HttpModule } from '@angular/http';
+import { SQLite } from '@ionic-native/sqlite';
+
 
 @NgModule({
   declarations: [
     MyApp,
     Timers,
-    TimerDetail,
+    EditTimer,
     AddTimer
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp),
+    HttpModule,
+    IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     Timers,
-    TimerDetail,
+    EditTimer,
     AddTimer
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    TimerService,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    SQLite,
+    DBService
   ]
 })
 export class AppModule {}
